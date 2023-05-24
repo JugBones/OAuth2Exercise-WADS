bd27efabf9c9ee31a0ed7482ae838da1a40699bf
JugBones <christopher.tjiandra@binus.ac.id>
2023-05-24 20:40:26 +0700

    try to connect the frontend and backend

--- FrontEnd/src/Login.js
+++ FrontEnd/src/Login.js
@@ -1,24 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export const Login = (props) => {
-    const {email, setEmail} = useState('');
-    const {pass, setPass} = useState('');
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(email);
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send login request to the backend
+      const response = await axios.post('http://localhost:8000/api/login', {
+        email,
+        password,
+      });
+
+      // Handle successful login
+      console.log(response.data);
+    } catch (error) {
+      // Handle login error
+      console.error(error);
     }
-    return(
-    <div className='auth-form-container'>
-        <form onSubmit={handleSubmit}>
-            <label htmlfor="email">email</label>
-            <input value={email} type="email" placeholder="E-mail" id="email" name="email"></input>
-            <label htmlfor="password">password</label>
-            <input value={pass} type="password" placeholder="Password" id="password" name="password"></input>
-            <button type="submit">Log In</button>
-        </form>
-        <button onClick={() => props.onFormSwitch('register')}>Don't Have an Account yet? Register Here</button>
-    </div>
 
-    )
-}
+    console.log(email);
+  };
+
+  return (
+    <div className='auth-form-container'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>Email</label>
+        <input
+          value={email}
+          type='email'
+          placeholder='E-mail'
+          id='email'
+          name='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          value={password}
+          type='password'
+          placeholder='Password'
+          id='password'
+          name='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>Log In</button>
+      </form>
+      <button onClick={() => props.onFormSwitch('register')}>
+        Don't Have an Account yet? Register Here
+      </button>
+    </div>
+  );
+};
+
+export default Login;
